8f5dac2cecf50b55d704e820a2e95d0b5108492e
Orry Mevorach <orry.mevorach@loblaw.ca>
2019-04-25 11:25:07 -0400

    Revert "created counter tech challenge, complete and incomplete versions"
    This reverts commit 5d1a812755b11fc00f97a915eba6384ad0cfe2a1.

--- src/CounterComplete/Reset.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Reset = (props) => {
-    return (
-        <div className="block reset" onClick={props.reset}>Reset</div>
-    )
-}
-
-export default Reset ;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "./scss/main.scss";
-import CounterComplete from './CounterComplete/CounterComplete';
-import Counter from './CounterTECHTEST/Counter';
 
 class App extends React.Component {
     render() {
         return (
             <div>
-                {/* <CounterComplete /> */}
-                <Counter />
+                Hello World
             </div>
         )
     }
